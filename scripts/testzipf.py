40f0ff008dd1584a1332e1218bda941e3f6d87d5
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-05-28 10:24:28 -0300

    modificando linha 21 para não atraplhar resultado.txt - devido criacao da pasta data

--- scripts/testzipf.py
+++ scripts/testzipf.py
@@ -18,6 +18,6 @@ if __name__ == '__main__':
     for input_file in input_files:
         counts = load_word_counts(input_file)
         [first, second] = top_two_word(counts)
-        bookname = input_file[:-4]
+        bookname = input_file[:-4].split('/')[1]
         print("%s\t%i\t%i\t%.2f" %(bookname, first, second, float(first)/second))
 
