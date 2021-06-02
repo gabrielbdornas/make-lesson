dc6476f45ebe1521a7007237f1cb8291e39e309a
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-06-02 09:41:03 -0300

    ajustes finais documentacao - correções necessárias

--- config.mk
+++ config.mk
@@ -2,9 +2,9 @@
 LANGUAGE=python
 
 # Count words script.
-COUNT_SRC=countwords.py
+COUNT_SRC=scripts/countwords.py
 COUNT_EXE=$(LANGUAGE) $(COUNT_SRC)
 
 # Test Zipf's rule
-ZIPF_SRC=testzipf.py
+ZIPF_SRC=scripts/testzipf.py
 ZIPF_EXE=$(LANGUAGE) $(ZIPF_SRC)
